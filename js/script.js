02483a83f204121b6d108c4dc52e32dd7338f992
Flurry2010 <Flurry2010@gmail.com>
2016-03-08 22:34:44 +0100

    Flurrys quatsch

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
- 'use strict'
+/**
+ * Created by dfleuren on 24.02.2016.
+ */
+"use strict";
+
+var audio = document.getElementById('sound');
+var button = document.getElementById('mute');
+
+mute.addEventListener('click', function (e) {
+    if (audio.paused) {
+        audio.play();
+    }else{
+        audio.pause();
+        audio.currentTime = 0
+    }
+}
 
