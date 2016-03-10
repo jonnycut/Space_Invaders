0adc6d46b9cf7a96ec05265736798292aae17f73
Flurry2010 <flurry2010@gmail.com>
2016-03-10 07:17:23 +0100

    flurrys quatsch

--- js/script.js
+++ js/script.js
@@ -4,37 +4,33 @@
 "use strict";
 
 
-
 /* Pausiert die Hintergrundmusik der Webseite und wechselt das Button-Logo*/
- function pausieren(){
+function pausieren(){
     var lala = document.getElementById("sound");
     if(lala.paused){
-      lala.play();
-      document.getElementById("mute").src = "images/unmute.png";
-      }
-     else{
-      lala.pause();
-      document.getElementById("mute").src = "images/mute.png";
-      }
-  }
+        lala.play();
+        document.getElementById("mute").src = "images/unmute.png";
+    }
+    else{
+        lala.pause();
+        document.getElementById("mute").src = "images/mute.png";
+    }
+}
 
- function play(){
+function play(){
     document.getElementById("titlecontent").style.display = "none";
     document.getElementById("start").style.display = "block";
- }
+}
 
- function game(){
+function game(){
     document.getElementById("start").style.display = "none";
     document.getElementById("arcade").style.display = "block";
     document.getElementById("design").style.display = "block";
- }
+}
 
- function anzeigen(){
+function anzeigen(){
     document.getElementById("design").style.display = "block";
     document.getElementById("anleitung").style.display = "block";
     document.getElementById("hilfe").style.display = "block";
     document.getElementById("credits").style.display = "block";
- }
-
-
-
+}
