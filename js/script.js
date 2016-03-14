3513db7831a34e8ce3be5a7de7bce83add8c71ec
Flurry2010 <flurry2010@gmail.com>
2016-03-14 13:23:13 +0100

    flurrys quatsch

--- js/script.js
+++ js/script.js
@@ -5,42 +5,60 @@
 
 
 /* Pausiert die Hintergrundmusik der Webseite und wechselt das Button-Logo*/
-function pausieren(){
+function pausieren() {
     var lala = document.getElementById("sound");
-    if(lala.paused){
+    if (lala.paused) {
         lala.play();
         document.getElementById("mute").src = "images/unmute.png";
     }
-    else{
+    else {
         lala.pause();
         document.getElementById("mute").src = "images/mute.png";
     }
 }
 
-function egg(){
+function egg() {
     document.getElementById("flurry").style.display = "block";
     document.getElementById("flurry").style.backgroundColor = "none";
     document.getElementById("egg").style.display = "none";
-    setTimeout(function(){document.getElementById("flurry").style.display = "none"},14500)
+    setTimeout(function () {
+        document.getElementById("flurry").style.display = "none"
+    }, 14500)
 }
 
 
-function play(){
+function play() {
     document.getElementById("titlecontent").style.display = "none";
     document.getElementById("start").style.display = "block";
     document.getElementById("sound").pause();
 }
 
-function game(){
+function game() {
     document.getElementById("start").style.display = "none";
     document.getElementById("arcade").style.display = "block";
-    setTimeout(function(){document.getElementById("design").style.display = "block";},600)
+    setTimeout(function () {
+        document.getElementById("design").style.display = "block";
+    }, 600)
     document.getElementById("fx").play();
 }
 
-function anzeigen(){
-    document.getElementById("design").style.display = "block";
-    document.getElementById("anleitung").style.display = "block";
-    document.getElementById("hilfe").style.display = "block";
-    document.getElementById("credits").style.display = "block";
+function anzeigen() {
+    if ((document.getElementById("credits").style.display = "block") || (document.getElementById("hilfe").style.display = "block")) {
+        document.getElementById("titlecontent").style.display = "none";
+        document.getElementById("anleitung").style.display = "block";
+        document.getElementById("credits").style.display = "none";
+        document.getElementById("hilfe").style.display = "none";
+    }
+    if ((document.getElementById("anleitung").style.display = "block") || (document.getElementById("hilfe").style.display = "block")) {
+        document.getElementById("titlecontent").style.display = "none";
+        document.getElementById("credits").style.display = "block";
+        document.getElementById("anleitung").style.display = "none";
+        document.getElementById("hilfe").style.display = "none";
+    }
+    if ((document.getElementById("anleitung").style.display = "block") || (document.getElementById("credits").style.display = "block")) {
+        document.getElementById("titlecontent").style.display = "none";
+        document.getElementById("hilfe").style.display = "block";
+        document.getElementById("anleitung").style.display = "none";
+        document.getElementById("credits").style.display = "none";
+    }
 }
