dde74714bf89e037573abc34245a4cc5e27e1a6c
Flurry2010 <flurry2010@gmail.com>
2016-03-21 09:14:20 +0100

    Flurrys quatsch NEU

--- js/script.js
+++ js/script.js
@@ -2,3 +2,50 @@
  * Created by dfleuren on 24.02.2016.
  */
 "use strict";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pausieren der Hintergrundmusik
+function pausieren() {
+    var mute = document.getElementById("mute");
+    var lala = document.getElementById("backgroundSound");
+    if (lala.paused) {
+        lala.play();
+        mute.src = "images/unmute.png";
+    }
+    else {
+        lala.pause();
+        mute.src = "images/mute.png";
+    }
+}
