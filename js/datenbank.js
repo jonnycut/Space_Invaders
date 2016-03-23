3400b2fbc12397558be9ba47fde293d0defcf572
ehampel <enricohampel@yahoo.de>
2016-03-23 12:58:35 +0100

    Index 2 hinzugefügt wegen Problemen nach änderungen Weiter Button hinzugefügt DB Zugriff muss angepasst werden

---
+++ js/datenbank.js
@@ -0,0 +1,30 @@
+/**
+ * Created by ehampel on 23.03.2016.
+ */
+
+function holen() {
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('POST', 'datenbank.php', true);
+    xmlhttp.addEventListener('readystatechange', function () {
+
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            console.log(xmlhttp.responseText);
+        }
+
+    });
+
+    xmlhttp.send();
+}
+function send() {
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('POST', 'datenbank.php', true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.addEventListener('readystatechange', function () {
+
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            console.log(xmlhttp.responseText);
+        }
+
+    });
+    xmlhttp.send("user=" + encodeURIComponent(zustand.spielerName) + "&zeit=" + encodeURIComponent(time()) + "&punkte=" + encodeURIComponent(punkte()));
+}
