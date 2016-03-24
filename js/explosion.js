f1e5b775ef03f23a6ef9357ade0c2a733af2d350
Flurry2010 <flurry2010@gmail.com>
2016-03-24 13:26:20 +0100

    Flurrys BOOM

---
+++ js/explosion.js
@@ -0,0 +1,117 @@
+/**
+ * Created by dfleuren on 24.03.2016.
+ */
+'use strict'
+
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+var w = canvas.width;
+var h = canvas.height;
+var lager = [];
+
+
+//----------------------------------------------------------------------------
+
+function removeP(){
+    console.log("lösche")
+    for(var l = lager.length-1, i = l; i >=0 ;i--){
+        if(lager[i].life < 0){
+            lager[i] = lager[lager.length - 1];
+            lager.length--;
+        }
+    }
+}
+
+function particle(alienX, alienY) {
+    var _this = this;
+console.log("erstelle");
+        this.x = alienX;
+        this.y = alienY;
+        this.umfang = 0.5 + Math.random() * 3;
+        this.vx = -1.5 + Math.random() * 5;
+        this.vy = -1.5 + Math.random() * 5;
+
+        this.life = 5;
+
+        //test
+        this.x = w / 2;
+        this.y = h / 2;
+
+    this.update = function(){
+        this.x += this.vx;
+        this.y += this.vy;
+        this.life--;
+    }
+}
+
+function clear(){
+    ctx.clearRect(0,0,w,h)
+}
+
+
+function zeichne(){
+    ctx.fillStyle = "red";
+    removeP();
+console.log("zeichne")
+
+    for(let i = 0; i < lager.length; i++){
+        var p = lager[i];
+
+        ctx.fillRect(p.x-1, p.y-1, p.umfang, p.umfang);
+        p.update();
+    }
+}
+
+function fuellen() {
+    console.log("fülle");
+    for (let i = 0; i < 20; i++) {
+        lager[i] = new particle(700, 400);
+    }
+}
+
+var t;
+function update(){
+    if(t != null){
+        clearTimeout(t);
+    }
+    clear();
+    zeichne();
+
+    t = setTimeout(update, 33);
+}
+update();
+
+canvas.addEventListener("click", function(e) {
+
+    var x, y;
+    if (!e) e = window.event;
+
+    if (e.pageX || e.pageY)     {
+        x = e.pageX;
+        y = e.pageY;
+    }
+    if (e.clientX || e.clientY)     {
+        x = e.clientX;
+        y = e.clientY;
+    }
+
+    var p = canvas;
+    while(p.parentNode != null) {
+        x -= p.offsetLeft    - p.scrollLeft;
+        y -= p.offsetTop    - p.scrollTop;
+        p = p.parentNode;
+    }
+
+
+
+    fuellen(x, y);
+
+
+
+}, true);
+
+
+
+
+
+
