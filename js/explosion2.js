f3f13db79501167ff11a88c227dbaa82c45871ea
Flurry2010 <Flurry2010@gmail.com>
2016-03-25 09:31:08 +0100

    Flurrys  Quatsch

--- js/explosion2.js
+++ js/explosion2.js
@@ -1,3 +1,13 @@
 /**
  * Created by UFO on 03.2016.
  */
+
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+var lager = [];
+
+
+var Explosion = function () {
+
+
+}
