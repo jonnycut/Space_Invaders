727a003fa33774d046f6c430c626a83a4136c5f8
Flurry2010 <flurry2010@gmail.com>
2016-04-04 08:54:10 +0200

    Flurrys DB Anbindung

--- Datenbank/dbConfig.php
+++ Datenbank/dbConfig.php
@@ -6,14 +6,13 @@
  */
 
 // Verbindungsaufbau und Auswahl der Datenbank
-
 $db_host = "flurrysworld.dd-dns.de";
 $db_name = "db_ufo";
 $db_user = "ufo";
 $db_pass = "Spaceinvaders";
 $db_port = "3306";
 
-
+// Verbindungsdaten
 $db = mysql_connect($db_host+":"+$db_port,$db_user,$db_pass);
 if(!$db){
     exit("Es konnte keine Verbindung hergestellt werden!");
