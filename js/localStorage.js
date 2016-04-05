89452984011b37bdd1af90dc7a581959761de927
Flurry2010 <flurry2010@gmail.com>
2016-04-05 11:16:58 +0200

    Flurrys localStorage

--- js/localStorage.js
+++ js/localStorage.js
@@ -9,6 +9,7 @@ var playerData = {
     level: "80",
     live: "3",
     highscore: "0",
+    design: "",
     alien1: "0",
     alien2: "0",
     alien3: "0",
@@ -21,6 +22,7 @@ function getData() {
     playerData.level = spiel.gLevel;
     playerData.live = document.getElementById('l1').innerHTML;
     playerData.highscore = document.getElementById('score').innerHTML;
+    playerData.design = gewModus;
     playerData.alien1 = document.getElementById('a1').innerHTML;
     playerData.alien2 = document.getElementById('a2').innerHTML;
     playerData.alien3 = document.getElementById('a3').innerHTML;
@@ -39,6 +41,20 @@ function loadData() {
 }
 
 function setData() {
-
-
+    loadData();
+    if(playerData.name == "")
+    zustand.status = 2;
+    if(playerData.name != ""){
+        document.getElementById('name').innerHTML = playerData.name;
+        spiel.gLevel = playerData.level;
+        document.getElementById('l1').innerHTML = playerData.live;
+        document.getElementById('score').innerHTML = playerData.highscore;
+        gewModus = playerData.design;
+        document.getElementById('a1').innerHTML = playerData.alien1;
+        document.getElementById('a2').innerHTML = playerData.alien2;
+        document.getElementById('a3').innerHTML = playerData.alien3;
+        document.getElementById('a4').innerHTML = playerData.alien4;
+        document.getElementById('a5').innerHTML = playerData.alien5;
+        zustand.status = 3;
+    }
 }
