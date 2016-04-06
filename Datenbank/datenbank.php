09616d90d61d23f3d6bc2e2dcf8bd69542468f36
ehampel <enricohampel@yahoo.de>
2016-04-06 10:58:55 +0200

    Datenbankanbindung auf IP rennt, junge

--- Datenbank/datenbank.php
+++ Datenbank/datenbank.php
@@ -1,42 +1,25 @@
-/**
- * Created by UFO on 03.2016.
- */
+
 <?php
 
 // Verbindungsaufbau und Auswahl der Datenbank
-$db_host = "localhost";
-$db_name = "db_space";
-$db_user = "postgres";
-$db_pass = "root";
-$db_port = "5432";
 
-// Verbindungsdaten
-$db = pg_connect("host=localhost dbname=db_space user=postgres password=root")
+$db = pg_connect("host=192.168.40.9 port=5432 dbname=db_space user=postgres password=root")
+//dsfkldfjkjsd
 or die ('Es konnte keine Verbindung hergestellt werden!'.pg_last_error());
-if($db){
-    echo("Verbindung steht");
-}
+
+
+
 //Nach Beendigung des Spiels füge die Spieldaten als Datensatz hinzu:
 if (isset($_POST['spieler'])) {
-    $spiel = "INSERT INTO t_highscore (name,score) VALUES ($1,$2)";
-    $result = pg_query_params($db, $spiel, array($_POST ["spieler"], $_POST ["score"])) or die('Abfrage fehlgeschlagen: ' . mysql_error());
+    $result = pg_query_params($db, 'INSERT INTO t_highscore (name,score) VALUES ($1,$2)', array($_POST ["spieler"], $_POST ["score"]))
+    or die('Abfrage fehlgeschlagen: ' . pg_last_error());
 
 } else {
 
 //Ansonsten Gib den Highscore als Array zurück
-    $highscore = pg_query("SELECT * FROM t_highscore ORDER BY score DESC LIMIT 10") or die('Abfrage fehlgeschlagen: ' . mysql_error());
+    $highscore = pg_query("SELECT * FROM t_highscore ORDER BY score DESC LIMIT 10") or die('Abfrage fehlgeschlagen: ' . pg_last_error());
     echo json_encode(pg_fetch_all($highscore));
-
-//Highscore resetten
-//    if (isset($_POST['user'])) {
-//        $reset = "DROP TABLE t_highscore";
-//        $neu = "CREATE TABLE t_highscore(name VARCHAR(30),zeit TIME,punkte DECIMAL)";
-//        pg_query($reset) or die('Abfrage fehlgeschlagen: ' . pg_last_error());
-//        pg_query($neu) or die('Abfrage fehlgeschlagen: ' . pg_last_error());
-//    }
-    // Speicher freigeben
-    pg_free_result($highscore);
 }
-// Verbindung schlieï¿½en
+
 pg_close($db);
 ?>
